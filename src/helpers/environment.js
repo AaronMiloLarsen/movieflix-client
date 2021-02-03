c0536ab5d78ea6002acffbd6212dd169490a2a02
Aaron Larsen <aaronmilolarsen@gmail.com>
2021-02-03 14:53:04 -0500

    adjusted env.js file

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -2,7 +2,9 @@ let APIURL = '';
 
 switch (window.location.hostname) {
     // this is the local host name of your react app
-    case 'localhost' || '127.0.0.1':
+    // case 'localhost' || '127.0.0.1':
+    case 'localhost':
+    case '127.0.0.1':
         // this is the local host name of your API
         APIURL = 'http://localhost:3500';
         break;
